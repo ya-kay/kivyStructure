3b206c136a65ea1a188c549b2972c504709f3257
yanik_deveb <ya-kay@gmx.de>
2020-01-30 20:00:54 +0100

    example addded

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivy.properties import ObjectProperty
 
 
 ## define own classes here
@@ -10,12 +11,21 @@ class LoadingScreen(Screen):
     pass
 
 class ExampleScreen(Screen):
-    pass
+    def run(self, name):
+        self.parent.current = "loading_screen"
+        # change screen from ExampleScreen Class
+
 
 class MainClass(App):
     ui = Builder.load_file("main.kv")
     
     def build(self):
         return self.ui
-    
+
+    def change_screen(self, screen_name):
+        screen_manager = self.root.ids["screen_manager"]
+        screen_manager.current = screen_name
+        # change screen from App Class
+
+
 MainClass().run()
